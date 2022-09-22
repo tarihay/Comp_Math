545b7d16528ba375295b2090ef4f597f1c3624b8
i.gorin <i.gorin@g.nsu.ru>
2022-09-22 19:09:36 +0700

    Fixed negative epsilon

--- src/main/java/FirstLab.java
+++ src/main/java/FirstLab.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class FirstLab {
-    private static final double negativeEpsilon = -0.01;
-
     private static final int FIRST_CASE = 1;
     private static final int SECOND_CASE = 2;
     private static final int THIRD_CASE = 3;
     private static final int FOURTH_CASE = 4;
+
+    private static final int MULTIPLICITY_ONE = 1;
+    private static final int MULTIPLICITY_TWO = 2;
+    private static final int MULTIPLICITY_THREE = 3;
 
     private static final double INFINITY = 10000000000.0;
     private static final double NEGATIVE_INFINITY = -10000000000.0;
@@ -14,7 +16,8 @@ public class FirstLab {
     private static final double ERROR = 12345.6;
 
     private static double epsilon;
-    private static double delta;
+    private static final double negativeEpsilon = -1*epsilon;
+    private static final double delta = 20;
     private static double a;
     private static double b;
     private static double c;
@@ -23,12 +26,10 @@ public class FirstLab {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please type epsilon: ");
         epsilon = scanner.nextDouble();
-        System.out.println("Please type delta: ");
-        delta = scanner.nextDouble();
         System.out.println("Now type 'a', 'b' and 'c': ");
-        a = scanner.nextLong();
-        b = scanner.nextLong();
-        c = scanner.nextLong();
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
 
         double aDif = 3;
         double bDif = 2 * a;
@@ -52,30 +53,46 @@ public class FirstLab {
             }
         }
         else {
-            double alpha = (0 - bDif/2 - Math.sqrt(discriminant))/aDif;
-            double betta = (0 - bDif/2 + Math.sqrt(discriminant))/aDif;
+            double alpha = (-1 * bDif/2 - Math.sqrt(discriminant))/aDif;
+            double betta = (-1 * bDif/2 + Math.sqrt(discriminant))/aDif;
 
             int alphaResult = funcValueCases(alpha);
             int bettaResult = funcValueCases(betta);
 
             if (alphaResult == FIRST_CASE && bettaResult == FIRST_CASE) {
-                System.out.println("The answer is: " + (alpha + betta)/2);
+                System.out.println("first case");
+                double result = (alpha + betta)/2;
+                System.out.println("The answer is: " + result + ". Multiplicity: 3");
             }
             if (alphaResult == THIRD_CASE && bettaResult == THIRD_CASE) {
-                System.out.println("The answer is: " + bisection(NEGATIVE_INFINITY, alpha));
+                System.out.println("second case");
+                double result = bisection(NEGATIVE_INFINITY, alpha);
+                System.out.println("The answer is: " + result + ". Multiplicity: " + checkMultiplicity(result));
             }
             if (alphaResult == SECOND_CASE && bettaResult == SECOND_CASE) {
-                System.out.println("The answer is: " + bisection(betta, INFINITY));
+                System.out.println("third case");
+                double result = bisection(betta, INFINITY);
+                System.out.println("The answer is: " + result + ". Multiplicity: " + checkMultiplicity(result));
             }
             if (alphaResult == THIRD_CASE && bettaResult == FIRST_CASE) {
-                System.out.println("The answers are: " + betta + " and " + bisection(NEGATIVE_INFINITY, alpha));
+                System.out.println("fourth case");
+                double result = bisection(NEGATIVE_INFINITY, alpha);
+                System.out.println("The answers are: " + betta + " and " + result +
+                        ". Multiplicities: " + checkMultiplicity(betta) + " and " + checkMultiplicity(result));
             }
             if (alphaResult == FIRST_CASE && bettaResult == SECOND_CASE) {
-                System.out.println("The answers are: " + alpha + " and " + bisection(betta, INFINITY));
+                System.out.println("fifth case");
+                double result = bisection(betta, INFINITY);
+                System.out.println("The answers are: " + alpha + " and " + result +
+                        ". Multiplicities: " + checkMultiplicity(alpha) + " and " + checkMultiplicity(result));
             }
             if (alphaResult == THIRD_CASE && bettaResult == SECOND_CASE) {
+                System.out.println("sixth case");
                 System.out.println("The answers are: " + bisection(NEGATIVE_INFINITY, alpha) +
-                        " and " + bisection(alpha, betta) + " and " + bisection(betta, INFINITY));
+                        " and " + bisection(alpha, betta) + " and " + bisection(betta, INFINITY) + ". Multiplicities: 1");
+            }
+            if (alphaResult == FOURTH_CASE || bettaResult == FOURTH_CASE) {
+                System.out.println("Something went wrong");
             }
         }
     }
@@ -161,5 +178,33 @@ public class FirstLab {
 
         }
         return c;
+    }
+
+    private static int checkMultiplicity(double arg) {
+        boolean isFuncNull = checkNullFunc(Math.round(arg));
+        boolean isFirstNull = checkNullFirstDerivative(Math.round(arg));
+        boolean isSecondNull = checkNullSecondDerivative(Math.round(arg));
+
+        if (isFuncNull && isFirstNull && isSecondNull) {
+            return MULTIPLICITY_THREE;
+        }
+        else if (isFuncNull && isFirstNull) {
+            return MULTIPLICITY_TWO;
+        }
+        else {
+            return MULTIPLICITY_ONE;
+        }
+    }
+
+    private static boolean checkNullFunc(long arg) {
+        return arg * arg * arg + a * arg * arg + b * arg + c == 0;
+    }
+
+    private static boolean checkNullFirstDerivative(long arg) {
+        return 3*arg*arg + 2*a*arg + b == 0;
+    }
+
+    private static boolean checkNullSecondDerivative(long arg) {
+        return 6*arg + 2*a == 0;
     }
 }
